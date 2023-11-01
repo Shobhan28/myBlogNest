21b3ff086296c839af537115e758080f8b2fc517
shobhan28 <shobhan28m@gmail.com>
2023-11-01 15:40:53 +0530

    first commit

---
+++ src/main/java/com/myBlogNest/MyBlogNestApplication.java
@@ -0,0 +1,13 @@
+package com.myBlogNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogNestApplication.class, args);
+	}
+
+}
