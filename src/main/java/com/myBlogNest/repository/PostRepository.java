912b78d3138fc7e9b2ce45c804febf2210da9f00
shobhan28 <shobhan28m@gmail.com>
2023-11-02 00:34:09 +0530

    forgot to give entity class name in generics

--- src/main/java/com/myBlogNest/repository/PostRepository.java
+++ src/main/java/com/myBlogNest/repository/PostRepository.java
@@ -1,8 +1,9 @@
 package com.myBlogNest.repository;
 
+import com.myBlogNest.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository {
+public interface PostRepository extends JpaRepository <Post, Long>{
 
 
 }
