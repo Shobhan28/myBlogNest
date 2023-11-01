fbe022f7473eb56701dc2673fbaba8b1d32af825
shobhan28 <shobhan28m@gmail.com>
2023-11-01 15:59:14 +0530

    created post fields

---
+++ src/main/java/com/myBlogNest/entity/Post.java
@@ -0,0 +1,31 @@
+package com.myBlogNest.entity;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+
+@Data
+@Entity
+@Table (name = "posts_table")
+public class Post {
+
+    // The unique identifier for each post
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    // The title of the blog post, which is required and mapped to the "title" column
+    @Column(name="title",nullable=false)
+    private String title;
+
+    // The description of the blog post, which is required and mapped to the "description" column
+    @Column(name="description", nullable=false)
+    private String description;
+
+    // The content of the blog post, which is required and mapped to the "content" column
+    @Column(name="content", nullable=false)
+    private String content;
+
+}
