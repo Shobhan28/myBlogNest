dba9ce97cc35044e6397d66f255d5369284c4840
shobhan28 <shobhan28m@gmail.com>
2023-11-02 11:29:53 +0530

    implemented many-to-many mapping

--- src/main/java/com/myBlogNest/entity/User.java
+++ src/main/java/com/myBlogNest/entity/User.java
@@ -4,7 +4,7 @@ package com.myBlogNest.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import java.util.Set;
 import javax.persistence.*;
 
 
@@ -29,4 +29,12 @@ public class User {
     private String password;
 
 
+    @ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @JoinTable(name="user_role",
+            joinColumns = @JoinColumn(name="user_id",referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id",referencedColumnName = "id")
+    )
+    private Set<Role> roles;
 }
+
+
