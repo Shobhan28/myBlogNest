633dd29e90d2b88a2f496dfe9a791396c516c759
shobhan28 <shobhan28m@gmail.com>
2023-11-02 11:47:05 +0530

    aligned lines

--- src/main/java/com/myBlogNest/controller/CommentController.java
+++ src/main/java/com/myBlogNest/controller/CommentController.java
@@ -24,7 +24,7 @@ public class CommentController {
 
     // http://localhost:8080/api/comments/{postId}
     @PostMapping("/{postId}")
-    public ResponseEntity<CommentDto> saveComment(@RequestBody CommentDto commentDto, @PathVariable long postId){
+    public ResponseEntity<CommentDto> saveComment(@RequestBody CommentDto commentDto, @PathVariable long postId) {
 
         // Call the CommentService to save the comment and associate it with the specified post.
         CommentDto commentDto1 = commentService.saveComment(commentDto, postId);
@@ -36,27 +36,27 @@ public class CommentController {
 
     // http://localhost:8080/api/comments/{commentId}
     @DeleteMapping("{commentId}")
-    public ResponseEntity<String> deleteById(@PathVariable long commentId){
+    public ResponseEntity<String> deleteById(@PathVariable long commentId) {
 
         commentService.deleteByCommentId(commentId);
 
-        return new ResponseEntity<>("Comment is deleted",HttpStatus.OK);
+        return new ResponseEntity<>("Comment is deleted", HttpStatus.OK);
     }
 
 
     // http://localhost:8080/api/comments/{commentId}
     @PutMapping("{id}")
-    public ResponseEntity<CommentDto> updateCommentId(@PathVariable long id,@RequestBody CommentDto commentDto){
-        CommentDto dto=commentService.updateCommentById(commentDto,id);
-        return new ResponseEntity<>(dto,HttpStatus.OK);
+    public ResponseEntity<CommentDto> updateCommentId(@PathVariable long id, @RequestBody CommentDto commentDto) {
+        CommentDto dto = commentService.updateCommentById(commentDto, id);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
 
     // http://localhost:8080/api/comments
     @GetMapping
-    public ResponseEntity<List<CommentDto>> getAllComments(){
-        List<CommentDto> listComment= commentService.getAllComment();
+    public ResponseEntity<List<CommentDto>> getAllComments() {
+        List<CommentDto> listComment = commentService.getAllComment();
 
-        return new ResponseEntity<>(listComment,HttpStatus.OK);
+        return new ResponseEntity<>(listComment, HttpStatus.OK);
     }
 }
