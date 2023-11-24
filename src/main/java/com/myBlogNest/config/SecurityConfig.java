17b72a7a6d052429c1072c880a6d340e32c51c72
shobhan28 <shobhan28m@gmail.com>
2023-11-24 10:57:08 +0530

    enhanced configure method

--- src/main/java/com/myBlogNest/config/SecurityConfig.java
+++ src/main/java/com/myBlogNest/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.myBlogNest.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,8 +22,11 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableGlobalMethodSecurity(prePostEnabled = true)  // Enable method-level security with prePost annotations
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private CustomUserServiceDetails userDetailsService;
+
     @Bean  // Bean to provide an instance of the PasswordEncoder
-    PasswordEncoder getEncoder(){
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
@@ -30,13 +35,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
     // Configure security settings for HTTP requests
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET,"/api/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
                 .anyRequest()
                 .authenticated()
@@ -44,9 +50,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
-
     // Bean to provide an instance of UserDetailsService
-    @Override
+/*   @Override
     @Bean
     protected UserDetailsService userDetailsService(){
         UserDetails user = User.builder().username("shubh").
@@ -57,15 +62,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .password(getEncoder()
                         .encode("passs")).roles("ADMIN").build();
         return new InMemoryUserDetailsManager(admin,user);
+}   */
+    //now we are not going to use In-memory Authentication here.
 
-        //wHAT IS IN MEMORY AUTHANTICATION
-        //WHEN IMAGINE 7-8 ROLES ARE THERE FIRST YOU WANT TO DO AUTHORIZAAATION WITHOUT DEALING WITH dATABASE
-        //THEN YOU CREATE OBJECT LIKE THIS WITH THE ROLES AND DO YOUR TESTING WITHOUT DATABASE
-        //LATTER ONWE CAN STORE OUR USERNAME AND PASSWORD IN OUR DATABASE
 
-        // In-memory authentication allows you to define users and roles without using a database.
-        // This is useful for testing and prototyping. Later, you can switch to a database-backed
-        // UserDetailsService when needed.
+    //WHAT IS IN MEMORY AUTHENTICATION
+    //WHEN IMAGINE 7-8 ROLES ARE THERE FIRST YOU WANT TO DO AUTHORIZATION WITHOUT DEALING WITH DATABASE
+    //THEN YOU CREATE OBJECT LIKE THIS WITH THE ROLES AND DO YOUR TESTING WITHOUT DATABASE
+    //LATTER ONE CAN STORE OUR USERNAME AND PASSWORD IN OUR DATABASE
 
+    // In-memory authentication allows you to define users and roles without using a database.
+    // This is useful for testing and prototyping. Later, you can switch to a database-backed
+    // UserDetailsService when needed.
+
+    @Override
+    protected void configure (AuthenticationManagerBuilder authBuilder ) throws Exception{
+        authBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 }
